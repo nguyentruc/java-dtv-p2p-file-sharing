fd2eb76597cc31561dc38d0c0b3410fa61c203d6
Nguyen Dinh Trung Truc <truc.ndtt@gmail.com>
2016-03-09 17:04:08 +0700

    add structure DTV.java Peer.Java Tracker.java UI.java

---
+++ dtv-file-sharing/src/dtv/Peer.java
@@ -0,0 +1,13 @@
+package dtv;
+
+public class Peer implements Runnable{
+
+	public Peer() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void run(){
+		
+	}
+
+}
---
+++ dtv-file-sharing/src/dtv/Tracker.java
@@ -0,0 +1,13 @@
+package dtv;
+
+public class Tracker implements Runnable{
+
+	public Tracker() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void run(){
+		
+	}
+
+}
