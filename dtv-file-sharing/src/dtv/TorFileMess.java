834719aa446d1d59df69e082c4c947b15fa32ce8
Nguyen Dinh Trung Truc <truc.ndtt@gmail.com>
2016-03-23 15:02:40 +0700

    update tor mess

--- dtv-file-sharing/src/dtv/TorFileMess.java
+++ dtv-file-sharing/src/dtv/TorFileMess.java
@@ -6,6 +6,7 @@ public class TorFileMess {
 	int type;
 	File tor = null;
 	String path = null;
+	String hashCode = null;
 	
 	public TorFileMess()
 	{
@@ -14,10 +15,11 @@ public class TorFileMess {
 		path = null;
 	}
 	
-	public TorFileMess(int type, File tor, String path)
+	public TorFileMess(int type, File tor, String path, String hash)
 	{
 		this.type = type;
 		this.tor = tor;
 		this.path = path;
+		this.hashCode = hash;
 	}
 }
